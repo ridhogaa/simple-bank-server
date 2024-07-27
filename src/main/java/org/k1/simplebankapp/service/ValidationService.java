b53c0063bc3b960f0df540299ed2d4a0df34f5bb
Ridho Gymnastiar <ridho.alrasyid@midasteknologi.com>
2024-07-27 17:07:58 +0700

    add common validation user and account

--- src/main/java/org/k1/simplebankapp/service/ValidationService.java
+++ src/main/java/org/k1/simplebankapp/service/ValidationService.java
@@ -1,5 +1,14 @@
 package org.k1.simplebankapp.service;
 
+import org.k1.simplebankapp.entity.Account;
+import org.k1.simplebankapp.entity.User;
+
+import java.security.Principal;
+
 public interface ValidationService {
     void validate(Object request);
+
+    User validateCurrentUser(Principal principal);
+
+    Account validateCurrentUserHaveThisAccount(Principal principal, String accountNo);
 }
